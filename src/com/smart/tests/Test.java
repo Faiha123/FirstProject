8fde8b9d4aaf98821f944461a22411b581b66232
Faiha123 <fmhoque@gmail.com>
2024-01-06 15:50:19 -0500

    commit 2

--- src/com/smart/tests/Test.java
+++ src/com/smart/tests/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	@org.testng.annotations.Test
 	public void loginTest() {
 		System.out.println("We are learning Git and GitHub through Eclipse IDE");
+		System.out.println("We had our initial commit");
 	}
 	
 	
